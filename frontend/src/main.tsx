25cd6f28fcb3cf34e8ec95156c972f75b2dcc291
Daan 298261 <29249777+Daan298261@users.noreply.github.com>
2026-09-08 18:33:04 +0200

    style(presence): load shared presence styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import { PresentationBootstrap } from "./presence/PresentationBootstrap"
 import "./index.css"
+import "./presence/presence.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
